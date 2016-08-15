a40d2a0070ca77c438c3b8a23f95072d01ccfc89
YouniS Bensalah <younis.bensalah@gmail.com>
2016-08-15 07:51:33 +0200

    Add and implement Markdown(Extra)RendererDecorator <3

---
+++ src/endobox/MarkdownExtraRendererDecorator.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of endobox.
+ *
+ * (c) 2015-2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace endobox;
+
+class MarkdownExtraRendererDecorator extends RendererDecorator
+{
+
+    public function render(Renderable $input, array &$data = null) : string
+    {
+        return \ParsedownExtra::instance()->text(parent::render($input, $data));
+    }
+
+}
