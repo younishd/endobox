00a3f9a53bb8b64eb085932211c9aa5ee32eddc2
YouniS Bensalah <younis.bensalah@gmail.com>
2016-08-15 06:39:46 +0200

    Add Template class structure

---
+++ src/endobox/Template.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of endobox.
+ *
+ * (c) 2015-2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace endobox;
+
+class Template implements Renderable
+{
+    private $filename;
+
+    public function __construct(string $filename)
+    {
+
+    }
+
+    public function __toString()
+    {
+
+    }
+
+    public function render()
+    {
+        
+    }
+
+}
