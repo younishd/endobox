5a319f2e408eab4fd6dd5edc088c9473e4dac7bb
YouniS Bensalah <younis.bensalah@gmail.com>
2016-08-15 07:51:00 +0200

    Add and implement RendererDecorator

---
+++ src/endobox/RendererDecorator.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of endobox.
+ *
+ * (c) 2015-2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace endobox;
+
+abstract class RendererDecorator implements Renderer
+{
+
+    private $renderer;
+
+    public function __construct(Renderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
+    public function render(Renderable $input, array &$data = null) : string
+    {
+        return $this->renderer->render($input, $data);
+    }
+
+}
