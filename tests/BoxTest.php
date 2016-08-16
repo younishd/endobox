f64db0d3b7e32ea069475335cb72c2ce852baeab
YouniS Bensalah <younis.bensalah@gmail.com>
2016-08-16 08:01:16 +0200

    Some unittests - still incomplete

--- tests/BoxTest.php
+++ tests/BoxTest.php
@@ -10,32 +10,83 @@
  */
 
 use \PHPUnit\Framework\TestCase;
+use \endobox\Engine;
 
 class BoxTest extends TestCase
 {
-
-    public function testSimpleRender()
+    protected function setUp()
     {
-        $endobox = new \endobox\endobox(__DIR__ . '/resources');
-        $box = $endobox('hello');
+        $this->engine = new Engine(__DIR__ . '/resources');
+    }
+
+    public function testSimpleStaticRender()
+    {
+        $box = $this->engine('hi');
         $result = $box->render();
-        $this->assertSame("<h1>Hello</h1>\n", $result);
+        $this->assertSame("<h1>Hi</h1>\n", $result);
+    }
+
+    public function testSimpleDynamicRender()
+    {
+        $box = $this->engine('hello');
+        $box->assign([ 'subject' => 'world' ]);
+        $result = $box->render();
+        $this->assertSame("<h1>Hello world</h1>\n", $result);
     }
 
     public function testChaining()
     {
-        $endobox = new \endobox\endobox(__DIR__ . '/resources');
-        $box = $endobox('first')('second')('third');
+        // simple append
+        $box = $this->engine('first')->append($this->engine('second'));
+        $this->assertSame("<p>First</p>\n<p>Second</p>\n", $result);
+
+        // simple prepend
+        $box = $this->engine('first')->prepend($this->engine('second'));
+        $this->assertSame("<p>Second</p>\n<p>First</p>\n", $result);
+
+        // chained append
+        $box = $this->engine('first')
+            ->append($this->engine('second'))
+            ->append($this->engine('third'));
         $result = $box->render();
         $this->assertSame("<p>First</p>\n<p>Second</p>\n<p>Third</p>\n", $result);
+
+        // chained prepend
+        $box = $this->engine('first')
+            ->prepend($this->engine('second'))
+            ->prepend($this->engine('third'));
+        $result = $box->render();
+        $this->assertSame("<p>Third</p>\n<p>Second</p>\n<p>First</p>\n", $result);
+
+        // __invoke()
+        $box = $this->engine('first')($this->engine('second'))($this->engine('third'));
+        $result = $box->render();
+        $this->assertSame("<p>First</p>\n<p>Second</p>\n<p>Third</p>\n", $result);
+
+        // append and prepend mixed
+        $box = $this->engine('first')
+            ->append($this->engine('second'))
+            ->prepend($this->engine('second'))
+            ->append($this->engine('first'))
+            ->prepend($this->engine('thrid'))
+            ->append($this->engine('third'));
+        $result = $box->render();
+        $this->assertSame(
+            "<p>Third</p>\n<p>Second</p>\n<p>First</p>\n<p>Second</p>\n<p>First</p>\n<p>Third</p>\n",
+            $result);
     }
 
     public function testAssignData()
     {
-        $endobox = new \endobox\endobox(__DIR__ . '/resources');
-        $box = $endobox('foobar');
+        // explicit via assign()
+        $box = $this->engine('foobar');
         $box->assign([ 'foo' => 'bar' ]);
         $result = $box->render();
+        $this->assertSame("<p>bar</p>\n", $result);
+
+        // implicit via render()
+        $box = $this->engine('foobar');
+        $result = $box->render([ 'foo' => 'bar' ]);
         $this->assertSame("<p>bar</p>\n", $result);
     }
 
---
+++ tests/resources/hello.php
@@ -0,0 +1,1 @@
+<h1>Hello <?php= $subject ?></h1>
