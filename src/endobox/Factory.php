ed65882250a8f86e1db7d21b541bc95a784482cb
YouniS Bensalah <younis.bensalah@gmail.com>
2016-08-19 10:03:03 +0200

    Implement Factory <3

--- src/endobox/Factory.php
+++ src/endobox/Factory.php
@@ -24,10 +24,7 @@ class Factory
      */
     public function __construct(string $path)
     {
-        if (!\is_dir($path)) {
-            throw new \RuntimeException(\sprintf('The path "%s" does not exist or is not a directory.', $path));
-        }
-        $this->paths[] = $path;
+        $this->add_folder($path);
     }
 
     /**
@@ -43,7 +40,39 @@ class Factory
      */
     public function make(string $template) : Box
     {
+        foreach ($this->paths as &$path) {
 
+            $file = \rtrim($path, '/') . '/' . \trim($template, '/');
+
+            if (\file_exists($t = $file . '.php')) {
+                return new Box(new Template($t), new EvalRendererDecorator(new NullRenderer()));
+            }
+
+            if (\file_exists($t = $file . '.md.php')) {
+                return new Box(new Template($t),
+                    new MarkdownRendererDecorator(new EvalRendererDecorator(new NullRenderer())));
+            }
+
+            if (\file_exists($t = $file . '.mdx.php')) {
+                return new Box(new Template($t),
+                    new MarkdownExtraRendererDecorator(new EvalRendererDecorator(new NullRenderer())));
+            }
+
+            if (\file_exists($t = $file . '.md')) {
+                return new Box(new Template($t), new MarkdownRendererDecorator(new NullRenderer()));
+            }
+
+            if (\file_exists($t = $file . '.mdx')) {
+                return new Box(new Template($t), new MarkdownExtraRendererDecorator(new NullRenderer()));
+            }
+
+            if (\file_exists($t = $file . '.html')) {
+                return new Box(new Template($t), new NullRenderer());
+            }
+
+        }
+
+        throw new \RuntimeException(\sprintf('Template "%s" not found.', $template));
     }
 
     /**
